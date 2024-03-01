e1d161f2b245bffe5ec2d597621063c856a805ad
Rewati123 <Rewatiraman130@gamil.com>
2024-03-01 18:13:13 +0530

    help page workinge

---
+++ src/app/user/help/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Help from '../../../Components/Help/help'
+
+const Helppage = () => {
+  return (
+    <div>
+
+
+        <Help/>
+    </div>
+  )
+}
+
+export default Helppage
